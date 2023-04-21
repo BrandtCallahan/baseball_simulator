bf7824fdded8d92dcc4f1ec517956b1df4478f4a
Brandt Callahan <74641699+BrandtCallahan@users.noreply.github.com>
2023-04-20 19:46:41 -0500

    Update baseball_reference.py

--- mlb_sim/baseball_reference.py
+++ mlb_sim/baseball_reference.py
@@ -487,16 +487,3 @@ def get_team_info(team_name, year_of_interest=None, team_soup=None):
                     continue
 
     return None, None
-
-date_abbreviations = {1: "Jan",
-                      2: "Feb",
-                      3: "Mar",
-                      4: "Apr",
-                      5: "May",
-                      6: "Jun",
-                      7: "Jul",
-                      8: "Aug",
-                      9: "Sep",
-                      10: "Oct",
-                      11: "Nov",
-                      12: "Dec"}
